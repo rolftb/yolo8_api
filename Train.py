8a7f4e00cb3a50f318cc03959fad2f3a9370b5b6
Rolf-Traeger <rolf.traeger@latam.com>
2023-12-09 18:40:14 -0300

    ADD: some basics

--- Train.py
+++ Train.py
@@ -6,4 +6,8 @@ model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data=r'vestidura-trabajo-vs.v2i.yolov8/data.yaml', epochs=25, imgsz=640)
+results = model.train(
+    data=r'vestidura-trabajo-vs.v2i.yolov8/data.yaml',
+    epochs=25,      # Number of epochs, i.e. cycles through the data (default: 3000) if low epochs, use high learning rate and vice versa
+    imgsz=640       # Image size (pixels) 640x640
+    )
